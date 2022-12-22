f3630b4f9f5cfaa33e4909d7e740c922365ddabd
Viktoria <v1k0808@yandex.ru>
2022-12-22 17:14:24 +0300

    test41

---
+++ test41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 41: Пользовательс клавиатуры вводит число M чисел.
+// Посчитайте, сколько чисел больше 0 ввел пользователь
+// 0, 7, 8, -2, -2 -> 2
+void CountPosNumbers(string[]listNum)
+{
+ int count = 0;
+ Console.Write("Введен следующий список чисел: ");
+ foreach (string elem in listNum)
+ {
+    if (int.Parse(elem) > 0) count ++;
+ }
+ Console.WriteLine($"\nКол-во чисел > 0 = {count}");
+}
+Console.WriteLine("Введите произвольный список целых чисел через пробел или запятую: ");
+string[] list = (Console.ReadLine()!).Split(' ',',');
+CountPosNumbers(list);
+
